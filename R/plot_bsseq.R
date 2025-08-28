0885aa7e3d95f8ebe83541bdc9758d121d909823
James Eapen <james.eapen@vai.org>
2025-08-28 11:13:06 -0400

    reorder guides, set default region count to 10k, increase dotsize

--- R/plot_bsseq.R
+++ R/plot_bsseq.R
@@ -8,40 +8,51 @@
 #' @param alpha The alpha level for the points
 #' @param outfile Optional file to write the plot to
 #'
-#' @importFrom ggplot2 ggplot aes geom_point stat_summary labs expand_limits scale_color_manual theme_bw
+#' @importFrom ggplot2 ggplot aes geom_point stat_summary labs expand_limits scale_color_manual theme_bw guides guide_legend
 #' @export
 plot_bsseq <- function(
   benchmark,
   files_or_regions = "files",
-  other_max_count = 10,
+  other_max_count = 10000,
   linewidth = 0.6,
-  dot_size = 0.5,
+  dot_size = 1.0,
   alpha = 0.8,
   outfile = NULL
 ) {
-
   x_axis <- ifelse(files_or_regions == 'files', "file_count", "region_count")
   cap_axis <- ifelse(files_or_regions == 'files', "region_count", "file_count")
   plot_data <- benchmark
-  if ( (files_or_regions == "files" & "region_count" %in% colnames(benchmark)) || (files_or_regions == "regions" & "region_count" %in% colnames(benchmark)) ) {
+  if (
+    (files_or_regions == "files" & "region_count" %in% colnames(benchmark)) ||
+      (files_or_regions == "regions" & "region_count" %in% colnames(benchmark))
+  ) {
     plot_data <- benchmark[get(cap_axis) == other_max_count]
   }
 
-  p <- ggplot(plot_data, aes(
-    x = get(x_axis),
-    y = time,
-    color = package,
-    linetype = thread_count,
-    shape = thread_count
-  )) +
+  plot_data[, thread_count := as.factor(thread_count)]
+
+  p <- ggplot(
+    plot_data,
+    aes(
+      x = get(x_axis),
+      y = time,
+      color = package,
+      linetype = thread_count,
+      shape = thread_count
+    )
+  ) +
     stat_summary(
-      aes(y = time, group = interaction(package, thread_count), linetype = thread_count),
+      aes(
+        y = time,
+        group = interaction(package, thread_count),
+        linetype = thread_count
+      ),
       fun = mean,
       geom = "line",
       linewidth = linewidth,
       show.legend = TRUE
     ) +
-    geom_point(alpha = alpha, show.legend = TRUE) +
+    geom_point(alpha = alpha, show.legend = TRUE, size = dot_size) +
     expand_limits(y = 0) +
     labs(
       y = "Runtime (seconds)",
@@ -49,6 +60,11 @@ plot_bsseq <- function(
       color = "Package",
       linetype = "Thread count",
       shape = "Thread count"
+    ) +
+    guides(
+      color = guide_legend(order = 1),
+      linetype = guide_legend(order = 2),
+      shape = guide_legend(order = 2)
     ) +
     package_colors +
     theme_bw()
