cb6bfe4d827263f2f9de2fba21be639a61bb6c52
James Eapen <james.eapen@vai.org>
2026-02-18 14:58:16 -0500

    fix(plot_runtime_diff): plot both sc and bulk on one file count

--- R/plot_runtime_diff.R
+++ R/plot_runtime_diff.R
@@ -1,23 +1,21 @@
-get_runtime_diff <- function(sc = TRUE, file_count = 90) {
-  if (sc) {
-    runtime_dt <- read_sc()
-  } else {
-    runtime_dt <- read_bulk()
-  }
+get_runtime_diff <- function(files = 100) {
+  runtime_dt_sc <- read_sc()[, exp_type := "single-cell"][]
+  runtime_dt_bulk <- read_bulk()[, exp_type := "bulk"][]
+  runtime_dt <- rbind(runtime_dt_sc, runtime_dt_bulk)[file_count == files]
 
   runtime_dt[,
     time.mean := mean(time),
-    by = .(package, file_count, thread_count)
+    by = .(package, file_count, thread_count, exp_type)
   ]
 
   iscream_means <- runtime_dt[
     package == 'iscream',
     .(iscream.min = min(time.mean)),
-    by = .(package, thread_count, file_count)
+    by = .(package, thread_count, file_count, exp_type)
   ]
-  (runtime_dt[iscream_means, on = .(thread_count, file_count)][,
+  (runtime_dt[iscream_means, on = .(thread_count, file_count, exp_type)][,
     time.relative := time.mean / iscream.min
-  ][, .(package, thread_count, file_count, time.relative)]) |>
+  ][, .(package, exp_type, thread_count, file_count, time.relative)]) |>
     unique()
 }
 
@@ -26,8 +24,9 @@ get_runtime_diff <- function(sc = TRUE, file_count = 90) {
 #' @importFrom ggplot2 ggplot aes geom_bar position_dodge2 labs scale_fill_manual theme_bw facet_grid theme element_text scale_y_continuous
 #' @import data.table
 #' @export
-plot_runtime_diff <- function(sc = TRUE, file_count = 90) {
-  rdiff <- get_runtime_diff(sc, file_count)
+plot_runtime_diff <- function(files = 100) {
+  rdiff <- get_runtime_diff(files)
+  rdiff <- get_runtime_diff(files)
   rdiff$package <- factor(
     rdiff$package,
     levels = c("iscream", "BSseq", "biscuiteer")
@@ -49,7 +48,7 @@ plot_runtime_diff <- function(sc = TRUE, file_count = 90) {
     ) +
     thread_colors_fill +
     theme_bw() +
-    facet_grid(~file_count, labeller = 'label_both') +
+    facet_grid(~exp_type) +
     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
     scale_y_continuous(breaks = seq(0, round(max(rdiff$time.relative)), 2))
 }
